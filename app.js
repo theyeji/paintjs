fb1896d3d515d2d272a086a71e5944a04189a808
theyeji <the-yeji@naver.com>
2021-03-18 15:37:12 +0900

    2.2 Recap

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -19,11 +22,11 @@ const onMouseMove = (event) => {
   const y = event.offsetY;
 
   if (!painting) {
-    ctx.beginPath();
-    ctx.moveTo(x, y);
+    ctx.beginPath(); // 경로 생성
+    ctx.moveTo(x, y); // 선 시작 좌표
   } else {
-    ctx.lineTo(x, y);
-    ctx.stroke();
+    ctx.lineTo(x, y); // 선 끝 좌표
+    ctx.stroke(); // 선 그리기
   }
 };
 
